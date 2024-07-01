650a9c1c7900087bb6c8ef7e340d68e1fba20b48
Felipe Mendes <lfelipecmendes@icloud.com>
2024-07-01 15:22:51 -0300

    feat: poo exercise solution add

---
+++ src/application/Program.java
@@ -0,0 +1,50 @@
+package application;
+
+import entities.Employee;
+import entities.OutsourcedEmployee;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        List<Employee> employees = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Employee #" + i + " data:");
+            System.out.print("Outsourced (y/n)? ");
+            char outsourced = sc.next().charAt(0);
+            sc.nextLine();
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if (outsourced == 'y') {
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                employees.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+            } else {
+                employees.add(new Employee(name, hours, valuePerHour));
+            }
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS:");
+        for (Employee employee : employees) {
+            sb.append(employee.getName()).append(" - $ ").append(String.format("%.2f", employee.payment())).append("\n");
+        }
+        System.out.println(sb);
+
+        sc.close();
+    }
+}
